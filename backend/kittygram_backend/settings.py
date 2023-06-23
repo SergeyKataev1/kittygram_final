ee68e0574302cdb99b4910d23c8267a95e96099e
SergayKataev1 <nimnul010@yandex.ru>
2023-06-24 00:35:50 +0300

    api не работает

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.72.66', '127.0.0.1', 'localhost', 'kitty-kitty.ddns.net']
+ALLOWED_HOSTS = ['158.160.72.66', '127.0.0.1', 'localhost', 'kitty-kitty.ddns.net', 'backend', '[::1]']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
